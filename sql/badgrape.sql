e0ca55c3e5d230d30143ba35225eecf73e80433b
badgrape <jimalexmorris@gmail.com>
2017-04-05 15:50:28 -0400

    Add files via upload

--- sql/badgrape.sql
+++ sql/badgrape.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2017 at 08:49 PM
+-- Generation Time: Apr 05, 2017 at 09:50 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.5.24
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `nthdb`
+-- Database: `badgrape`
 --
 
 -- --------------------------------------------------------
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(50) NOT NULL,
   `email` varchar(128) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -39,7 +39,14 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`userID`, `name`, `email`, `password`) VALUES
 (1, 'Jim', 'jim@badgrape.net', '523'),
-(3, 'Emily', 'ebraine@gmail.com', '');
+(3, 'Emily', 'ebraine@gmail.com', ''),
+(4, 'Jake', 'dirtyflash@hamster.net', 'jake'),
+(5, 'Roger', 'r2kidhacker@badgrape.net', 'dork'),
+(6, 'dBoon', 'd@minutemen.org', 'mikewatt'),
+(7, 'milo', 'milo@descendents.com', 'bill'),
+(8, 'Hossein', 'hossein@mybud.com', 'asdfads'),
+(9, 'flipper', 'generic@album.noise', 'flipoff'),
+(10, 'sparrowhawk', 'ged@earthsea.ea', '124d3feb470d61022a68343961b1b978');
 
 --
 -- Indexes for dumped tables
@@ -59,7 +66,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `userID` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
