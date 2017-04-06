9320642ecc7f09b0f1b2eedfd54ad05d85673396
badgrape <jim@badgrape.net>
2017-04-06 18:47:06 -0400

    updated db dump

--- sql/badgrape.sql
+++ sql/badgrape.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 05, 2017 at 09:50 PM
--- Server version: 5.6.24
--- PHP Version: 5.5.24
+-- Host: localhost
+-- Generation Time: Apr 06, 2017 at 06:46 PM
+-- Server version: 5.7.17-0ubuntu0.16.04.2
+-- PHP Version: 7.0.15-0ubuntu0.16.04.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `badgrape`
@@ -26,12 +26,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `userID` int(4) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(128) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -46,7 +46,8 @@ INSERT INTO `users` (`userID`, `name`, `email`, `password`) VALUES
 (7, 'milo', 'milo@descendents.com', 'bill'),
 (8, 'Hossein', 'hossein@mybud.com', 'asdfads'),
 (9, 'flipper', 'generic@album.noise', 'flipoff'),
-(10, 'sparrowhawk', 'ged@earthsea.ea', '124d3feb470d61022a68343961b1b978');
+(10, 'sparrowhawk', 'ged@earthsea.ea', '124d3feb470d61022a68343961b1b978'),
+(20, 'Victor', 'where@am.i', '9efd09478327b0372750ba6e6a788444');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `userID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
