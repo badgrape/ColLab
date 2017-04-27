bed3c9f70fb5ac2486efe035ff52ac0b811e0c67
badgrape <jimalexmorris@gmail.com>
2017-04-27 10:21:31 -0400

    Update dbConnect.php

--- php/dbConnect.php
+++ php/dbConnect.php
@@ -1,10 +1,8 @@
 <?php
 
 $db = "collab";
-//$user = "root";
-//$pass ="";
 $user = "jim";
-$pass = "s3ns3nn0s3n";
+$pass = "";
 
 $pdo = new PDO ("mysql:host=localhost;dbname=$db", $user, $pass);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
